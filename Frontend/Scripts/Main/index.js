3ea5246790df27c86e139c9b427e5ccbfa51ac50
Artemov Anton <anton.artemov2011@yandex.ru>
2018-07-15 22:20:49 +0300

    Добавлен favicon

--- Frontend/Scripts/Main/index.js
+++ Frontend/Scripts/Main/index.js
@@ -4,9 +4,6 @@ import App from './Containers/App';
 import stores from './Stores';
 
 import '../../Styles/Main/index.less';
-import '../../Images/favicon.ico';
-import '../../Images/favicon-16x16.png';
-import '../../Images/favicon-32x32.png';
 
 new AppService({
   App,
