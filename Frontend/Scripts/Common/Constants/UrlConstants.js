aa3cab337e790050a8255b27158efcb1e047c2ab
Artemov Anton <anton.artemov2011@yandex.ru>
2018-07-15 22:33:44 +0300

    Подготовка к запуску на heroku

--- Frontend/Scripts/Common/Constants/UrlConstants.js
+++ Frontend/Scripts/Common/Constants/UrlConstants.js
@@ -1,3 +1,2 @@
-// const prod = process.env.NODE_ENV === 'production';
-// export const API_URL = prod ? 'https://online-library-astral.herokuapp.com' : 'http://localhost:5000';
-export const API_URL = 'http://localhost:5000/api';
+const prod = process.env.NODE_ENV === 'production';
+export const API_URL = prod ? 'https://world-countries-astral.herokuapp.com/api' : 'http://localhost:5000/api';
